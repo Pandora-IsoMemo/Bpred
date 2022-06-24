d8163b19d5250940885690641c28747ab726cb59
Antonia Runge <antonia.runge@inwt-statistics.de>
2022-06-24 12:45:38 +0200

    Bug/7 clean up tests (#11)
    * fix tests
    
    * add packages for easy local testing
    
    * clean up tests
    
    * version up

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,12 +1,10 @@
-context("Plot estimates")
-
-test_that("plots creation", {
+testthat::test_that("plots creation", {
   load(testthat::test_path("testdata/test_plots.RData"))
   
-  g <- plotDensities(yEstimates, type = "Category", plotType = "KernelDensity")
+  g <- mpiBpred::plotDensities(yEstimates, type = "Category", plotType = "KernelDensity")
   
   
-  expect_type(g, "list")
-  expect_length(g, 9)
-  expect_equal(class(g), c("gg", "ggplot"))
+  testthat::expect_type(g, "list")
+  testthat::expect_length(g, 9)
+  testthat::expect_equal(class(g), c("gg", "ggplot"))
 })
--- tests/testthat/test-relationship.R
+++ tests/testthat/test-relationship.R
@@ -1,6 +1,4 @@
-context("compute relationship")
-
-test_that("computeResults simple example", {
+testthat::test_that("computeResults simple example", {
   set.seed(100)
   #create simulated data set
   n <- 100
@@ -12,20 +10,36 @@ test_that("computeResults simple example", {
   xobs <- x + (rnorm(n, sd = xunc))
   yobs <- y + (rnorm(n, sd = yunc))
   
-  f1 <- estimateRelationship(y = yobs, x = xobs, yunc = yunc, xunc = xunc, link = "linIntcp", chains = 2)
+  f1 <-
+    mpiBpred::estimateRelationship(
+      y = yobs,
+      x = xobs,
+      yunc = yunc,
+      xunc = xunc,
+      link = "linIntcp",
+      chains = 2
+    )
   
-  expect_type(f1, "list")
-  expect_length(f1, 5)
-  expect_equal(f1$type, 1)
-  expect_equal(mean(f1$slope), 1.51, tolerance = 0.01)
+  testthat::expect_type(f1, "list")
+  testthat::expect_length(f1, 5)
+  testthat::expect_equal(f1$type, 1)
+  testthat::expect_equal(mean(f1$slope), 1.51, tolerance = 0.01)
   
-  f2 <- estimateRelationship(y = yobs, x = xobs, yunc = yunc, xunc = xunc, link = "linNoIntcp", chains = 2)
-  expect_type(f2, "list")
-  expect_length(f2, 5)
-  expect_equal(f2$type, 2)
+  f2 <-
+    mpiBpred::estimateRelationship(
+      y = yobs,
+      x = xobs,
+      yunc = yunc,
+      xunc = xunc,
+      link = "linNoIntcp",
+      chains = 2
+    )
+  testthat::expect_type(f2, "list")
+  testthat::expect_length(f2, 5)
+  testthat::expect_equal(f2$type, 2)
 })
 
-test_that("computeResults simple example wrong link negative data", {
+testthat::test_that("computeResults simple example wrong link negative data", {
   #create simulated data set
   n <- 100
   y <- rnorm(n)
@@ -35,31 +49,56 @@ test_that("computeResults simple example wrong link negative data", {
   yunc <- rep(0.25, length(y))
   xobs <- x + (rnorm(n, sd = xunc))
   yobs <- y + (rnorm(n, sd = yunc))
-  expect_error(estimateRelationship(y = yobs, x = xobs, yunc = yunc, xunc = xunc, link = "sqrt", chains = 2))
+  testthat::expect_error(
+    mpiBpred::estimateRelationship(
+      y = yobs,
+      x = xobs,
+      yunc = yunc,
+      xunc = xunc,
+      link = "sqrt",
+      chains = 2
+    )
+  )
 })
 
-test_that("test positive data example", {
+testthat::test_that("test positive data example", {
   #create simulated data set
   set.seed(100)
   n <- 100
   y <- rnorm(n)
   x <- exp(rnorm(n))
-  y <- exp(- 0.5 + 0.2 * x + rnorm(n, sd = 0.25))
+  y <- exp(-0.5 + 0.2 * x + rnorm(n, sd = 0.25))
   xunc <- rep(0.1, length(x))
   yunc <- rep(0.1, length(y))
   xobs <- exp(log(x) + (rnorm(n, sd = xunc)))
   yobs <- exp(log(y) + (rnorm(n, sd = yunc)))
-  f1 <- estimateRelationship(y = yobs, x = xobs, yunc = yunc, xunc = xunc, link = "log", chains = 2)
+  f1 <-
+    mpiBpred::estimateRelationship(
+      y = yobs,
+      x = xobs,
+      yunc = yunc,
+      xunc = xunc,
+      link = "log",
+      chains = 2
+    )
   
-  expect_type(f1, "list")
-  expect_length(f1, 5)
-  expect_equal(f1$type, 4)
-  expect_equal(mean(f1$slope), 0.1175, tolerance = 0.01)
+  testthat::expect_type(f1, "list")
+  testthat::expect_length(f1, 5)
+  testthat::expect_equal(f1$type, 4)
+  testthat::expect_equal(mean(f1$slope), 0.1175, tolerance = 0.01)
   
-  f2 <- estimateRelationship(y = yobs, x = xobs, yunc = yunc, xunc = xunc, link = "sqrt", chains = 2)
+  f2 <-
+    mpiBpred::estimateRelationship(
+      y = yobs,
+      x = xobs,
+      yunc = yunc,
+      xunc = xunc,
+      link = "sqrt",
+      chains = 2
+    )
   
-  expect_type(f2, "list")
-  expect_length(f2, 5)
-  expect_equal(f2$type, 3)
-  expect_equal(mean(f2$slope), 1.388541e-05, tolerance = 5E-04)
+  testthat::expect_type(f2, "list")
+  testthat::expect_length(f2, 5)
+  testthat::expect_equal(f2$type, 3)
+  testthat::expect_equal(mean(f2$slope), 1.388541e-05, tolerance = 5E-04)
 })
