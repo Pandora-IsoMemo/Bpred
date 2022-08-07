b27ece4ce1e11c7281e710cb7fc1c736b955a327
mgross <marcus.gross@inwt-statistics.de>
2022-08-07 15:47:15 +0200

    paste error

--- inst/app/server.R
+++ inst/app/server.R
@@ -185,10 +185,10 @@ shinyServer(function(input, output, session) {
                                                                                                              sep = " = (", collapse = ",)")), collapse = ", ", ")"),
                          R_squared = signif(1 - sum((colMeans(res$yPredmc) - y)^2) / sum((y-mean(y))^2), 4),
                          p_direction = paste0(lapply(1:length(parNames),
-                                                     function(x) paste("(", parNames[x],
+                                                     function(x) paste(parNames[x],
                                                                        paste(signif(max(sum(formulas$objects[[input$formName]]$beta[,x] > 0),
                                                                                         sum(formulas$objects[[input$formName]]$beta[,x] < 0)) / length(formulas$objects[[input$formName]]$beta[,x]), 4), collapse = ","),
-                                                                       sep = " = ", collapse = "")), collapse = ", (", ")"))
+                                                                       sep = " = (", collapse = "")), collapse = ", ", ")"))
     formulas$f <- data.frame(rbind(formulas$f, form))
     functionsFit(formulas)
 })
